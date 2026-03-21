4be5f3d7fa2dc9cb3f885ff69e686c972ff6ee25
ausrgonzale <ausrgonzale@gmail.com>
2026-03-21 17:20:10 -0500

    Fix database configuration for Heroku

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -88,6 +89,13 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+DATABASES['default'].update(
+    dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True
+    )
+)
 
 
 # Password validation
