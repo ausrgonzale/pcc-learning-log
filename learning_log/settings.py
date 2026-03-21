0c72cb99c777f5c55aedbd69bf9654c9226eefe0
ausrgonzale <ausrgonzale@gmail.com>
2026-03-21 16:44:13 -0500

    Add STATIC_ROOT for Heroku

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -150,7 +150,10 @@ if cwd == '/app' or cwd[:4] == '/tmp':
 
     # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'))
+    
+    
   
 
